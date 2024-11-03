cf41e6a406c38281f24576fcaa3269ea56cd3b87
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-11-03 23:09:38 +0500

    as

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -19,11 +19,11 @@ const Auth = () => {
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Login to admin panel
-                        </h1>
+                        </h1>   
                         <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleLogin}>
                             <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your login</label>
-                                <input type="text" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Login" required />
+                                <label htmlFor="login" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your login</label>
+                                <input type="text" name="login" id="login" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Login" required />
                             </div>
                             <div>
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
