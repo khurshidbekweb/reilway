3a6668063c0bdefe3b5c0df2a8502327ecee0cf8
khurshidbekweb <khurshidbeknuriddinov@gmail.com>
2024-11-12 03:00:55 +0500

    add select

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,6 +7,7 @@ const Navbar = () => {
         <div className="max-w-7xl mx-auto px-2">
             <div className=" flex justify-between items-center my-2">
                 <h1 className="text-2xl font-serif">REIWAY</h1>
+                
                 <Button>Log out</Button>
             </div>
             <Separator/>
